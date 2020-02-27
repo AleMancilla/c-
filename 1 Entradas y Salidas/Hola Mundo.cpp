e0ef8412cb06647488ab86c7b06b5d0c9ae3d957
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-26 22:38:01 -0400

    primer hola mundo en c++

---
+++ 1 Entradas y Salidas/Hola Mundo.cpp
@@ -0,0 +1,13 @@
+// primer programa en c++
+
+#include<iostream> // entrada y salida de datos
+
+using namespace std; // para no escribir std::cout<<
+//estandar de c++
+
+int main(){
+	
+	cout<<"Hola mundo :D"<<endl;
+	
+	return 0;
+}
