2d9430b69654a4c0aa56f1024fce96d073bd88fd
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-27 14:50:10 -0400

    ciclos repetitivos y sentencias de pausa al terminar el programa

---
+++ 2 condicionales y ciclos/do while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+	int i=0;
+	do{
+		cout<<"numero: "<<i<<endl;
+		i++;
+	}while(i<=10);
+	
+	system("pause");
+	return 0;
+}
---
+++ 2 condicionales y ciclos/while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int i = 0;
+	while(i<=10){
+		cout<<"Numero: "<<i<<endl;
+		i++;
+	}
+	getch();
+	return 0;
+}
