ca6ffa2d54b3a8c3c6d6309eb52bd106473442d6
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-26 23:08:05 -0400

    segundo ejercicio c++

---
+++ 1 Entradas y Salidas/ejercicio 2.cpp
@@ -0,0 +1,22 @@
+// entrada edad sexo y altura
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int edad;
+	char sexo[10];
+	float altura;
+	
+	cout<<"Digite su edad: "; cin>>edad;
+	cout<<"Digite su sexo: "; cin>>sexo;
+	cout<<"Digite su altura: "; cin>>altura;
+	
+	cout<<"\nEdad:"<<edad<<endl;
+	cout<<"Sexo: "<<sexo<<endl;
+	cout<<"Altura: "<<altura;
+	
+	return 0;
+}
