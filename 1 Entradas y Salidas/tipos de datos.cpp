f501216b6b737e3038f1862e3970b04a5dca347f
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-26 22:42:50 -0400

    tipos de datos basicos en c++

---
+++ 1 Entradas y Salidas/tipos de datos.cpp
@@ -0,0 +1,18 @@
+// tipos de datos basicos de c++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int entero = 15; // variable y operador de asignacion
+	float flotante = 10.45;
+	double mayor = 16.3456;
+	char letra = 'A';
+	
+	
+	cout<<letra;
+	
+	return 0;
+}
